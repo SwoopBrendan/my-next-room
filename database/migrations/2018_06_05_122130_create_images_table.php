d237f7b312976f68b2e9c6c4e6a5722f72bb5853
Brendan <brendan.vandermeulen@double-eye.com>
2018-06-05 14:23:03 +0200

    Initial room_images and images tables and models created

--- database/migrations/2018_06_05_122130_create_images_table.php
+++ database/migrations/2018_06_05_122130_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoomImagesTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateRoomImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('room_images', function (Blueprint $table) {
-            $table->integer('room_id')->unsigned();
-            $table->integer('image_id')->unsigned();
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image_path');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +27,6 @@ class CreateRoomImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('room_images');
+        Schema::dropIfExists('images');
     }
 }
