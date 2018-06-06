a343fd742054a10e3947b084da95f016614ca5bb
Brendan <brendan.vandermeulen@double-eye.com>
2018-06-06 12:38:05 +0200

    locations seeder and DB updates

--- database/migrations/2018_06_06_054812_create_greater_areas_table.php
+++ database/migrations/2018_06_06_054812_create_greater_areas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocationsTable extends Migration
+class CreateGreaterAreasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateLocationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('locations', function (Blueprint $table) {
+        Schema::create('greater_areas', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('slug');
-            $table->integer('postal_code')->unsigned();
-            $table->integer('greater_area_id')->unsigned();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateLocationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('locations');
+        Schema::dropIfExists('greater_areas');
     }
 }
