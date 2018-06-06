59fbd80ad5ef7a3e8f17f20b3a227817b997a98b
Brendan <brendan.vandermeulen@double-eye.com>
2018-06-06 06:57:44 +0200

    post install command and requirements table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(ExtrasTableSeeder::class);
+         $this->call(RequirementsTableSeeder::class);
     }
 }
